ebbf874045f1ae7bcbf4831b43699cc8b2e1e7e7
Nick Abal <nickabal@gmail.com>
2014-11-30 08:11:09 +0100

    root red

--- root/etc/profile.d/nixosPS1.sh
+++ root/etc/profile.d/nixosPS1.sh
@@ -1,2 +1,12 @@
 #add nixos green prompt
+
+if (( "$UID" == "0" )); then
+
+export PS1='\[\033]2;\h:\u:\w\007\]\n\[\033[1;31m\][\u@\h:\w]\$\[\033[0m\]'
+
+else
+
 export PS1='\[\033]2;\h:\u:\w\007\]\n\[\033[1;32m\][\u@\h:\w]\$\[\033[0m\]'
+
+fi
+
